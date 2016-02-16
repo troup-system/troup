efbe6a7b6050a7dd3deaa3379fd43025f124fb8f
Pavle Jonoski <pavle.jonoski@gmail.com>
2016-02-16 01:30:37 +0100

    System stats

--- appliance/system.py
+++ appliance/system.py
@@ -1,25 +1,42 @@
 __author__ = 'pavle'
 
-import psuti
+import psutil
 import os
-import threading
 
+from appliance.threading import IntervalTimer
 
 class SystemStats:
-    """
-    Stats:
-        - cpu usage
-        - memory usage
-        - total number of physical CPUs
-        - usage per CPU
-        - system load
-        - io load
+    """Statistics values for the underlying system.
+    
+    Holds the values for a single measurement of the system performance.
+    
+    Attributes:
+        cpu (dict): Holds the CPU values in a dictionary. The following values 
+            are retrieved: 
+            * "usage" (number): CPU usage. It is a number 0.0 - 1.0.
+            * "per_cpu" (Array[number]): CPU usage per CPU. Reports the usage of each
+                CPU separately in an Array.
+            * "processors" (int): Number of processors present on the system.
+            * "bogomips" (number): Bogus MIPS as reported by the Linux kernel (if available)
+        memory (dict): Holds the values for the system memory:
+            * "total" (int): total memory present in the system in Bytes.
+            * "user" (int): Used memory (in Bytes).
+            * "free" (int): Free memory (in Bytes).
+        system (dict): System load and general system info:
+            * "load" (Array[number]): System load as reported by the system. Usually this 
+                is an array of three values - the system load for the last 5, 10 and 15 minutes.
+            * "name" (str): Node name or the name of the machine.
+            * "platform" (str): The underlying OS platform. May be "linux", "bsd" etc.
+        disk (dict): Information about the disk and I/O of the system:
+            * "ioload" (number): Normalized Input/Output load of the system - a number
+                between 0.0 and 1.0. This is the average value for the disk usage in a 
+                previous time interval.
     """
 
     def __init__(self):
-        self.cpu = {'usage': 0.0, 'processors': 1, 'bogomips': 1}
-        self.memory = {'total': 1, 'used': 1, 'free': 1}
-        self.system = {'load': [0.0, 0.0, 0.0], 'name': ''}
+        self.cpu = {'usage': 0.0, "per_cpu": [], 'processors': 0, 'bogomips': 0}
+        self.memory = {'total': 0, 'used': 0, 'free': 0}
+        self.system = {'load': [0.0, 0.0, 0.0], 'name': '', 'platform': ''}
         self.disk = {'ioload': 0.0}
                 
 
@@ -30,14 +47,25 @@ class StatsTracker:
         self.cpu_usage = []
         self.cpu_usage_avg = psutil.cpu_percent()/100
         self.cpu_count = psutil.cpu_count()
-        
-        
+        self.periodic_update = IntervalTimer(interval=self.period, target=self.refresh_values)
+        self.periodic_update.start()
     
     def refresh_values(self):
         cpu_usage = psutil.cpu_percent(interval=self.period/1000,percpu=True)
         self.cpu_usage = [usage/100 for usage in cpu_usage]
     
+    def get_stats(self):
+        stats = SystemStats()
+        stats.cpu['usage'] = self.cpu_usage.avg
+        stats.cpu['per_cpu'] = self.cpu_usage
+        stats.cpu['processors'] = self.cpu_count
+        stats.cpu['bogomips'] = 0.0 # FIXME: read from proc
+        
+        
+        return stats
     
+    def stop_tracking(self):
+        self.periodic_update.cancel()
 
 
 
--- appliance/threading.py
+++ appliance/threading.py
@@ -1,4 +1,37 @@
 import threading
 
 class IntervalTimer:
-    pass
+    
+    def __init__(self, interval, offset=0, target=None):
+        self.target = target
+        self.timer = None
+        self.running = False
+        self.offset = offset
+        self.interval = interval
+        self.first = True
+        
+    def start(self):
+        if self.running:
+            return
+        interval = self.interval
+        if self.first:
+            interval = interval + self.offset
+            self.first = False
+        self.timer = threading.Timer(interval/1000, self._run_)
+        
+        
+    def cancel(self):
+        if self.timer:
+            self.timer.cancel()
+            self.running = False
+    
+    
+    def _run_(self):
+        self.running = True
+        self.run()
+        self.running = False
+    
+    def run(self):
+        if self.target:
+            self.target()
+    
