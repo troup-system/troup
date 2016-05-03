349042ed40a230b631605a10f6b834a9493fff15
Pavle Jonoski <jonoski.pavle@gmail.com>
2016-05-03 23:38:29 +0200

    Distributed Promise. Command API.

--- troup/client.py
+++ troup/client.py
@@ -2,61 +2,62 @@ from troup.infrastructure import OutgoingChannelOverWS
 
 
 
-class CommandClient:
-    
-    def __init__(self, nodes_specs=None):
+class ChannelClient:
+
+    def __init__(self, nodes_specs=None, reply_timeout=5000):
         self.nodes_ref = {}
         self.channels = {}
-        
+
         self.__build._nodes_refs__(nodes_specs)
-    
+
     def __build_nodes_refs__(self, nodes_specs):
         for spec in nodes_specs:
             parsed = spec.partition(':')
             self.nodes_ref[parsed[0]] = parsed[2]
-    
-    def send_command(self, command, to_node=None, on_reply=None):
+
+    def send_message(self, message, to_node=None, on_reply=None):
         def reply_callback_wrapper(*args, **kwargs):
             if on_reply:
                 on_reply(*args, **kwargs)
-                
+
         if to_node:
-            self.send_command_to_node(command, to_node, reply_callback_wrapper)
+            self.send_message_to_node(message, to_node, reply_callback_wrapper)
         else:
             for name, node in self.nodes_ref.items():
-                self.send_command_to_node(command, node, reply_callback_wrapper)
-    
-    def send_command_to_node(self, command, node, on_reply):
+                self.send_message_to_node(message, node, reply_callback_wrapper)
+
+    def send_message_to_node(self, message, node, on_reply):
         channel = self.get_channel(node)
-        channel.send(command)
-        
-    
+        channel.send(message)
+
+
     def get_channel(self, for_node):
         channel = self.channel.get(for_node)
         if not channel:
             channel = self.build_channel(for_node)
         return channel
-    
+
     def build_channel(self, for_node):
         ref = self.nodes_ref.get(for_node)
         if not ref:
             raise Exception('Unknown node reference [%s]' % for_node)
         return self.create_channel(for_node, ref)
-    
+
     def create_channel(node_name, reference):
         return OutgoingChannelOverWS(node_name, reference)
-    
-    
 
-class Job:
-    def __init__(self):
+
+
+class CommandAPI:
+
+    def __init__(self, channel_client):
+        self.channel_client = channel_client
+
+    def send_command(self, command, to_node=None, on_reply=None):
         pass
 
-class Client:
-    
-    def __init__(self):
-        pass
-    
-    def submit_job(self, job):
+    def monitor(self, command_ref):
         pass
 
+    def command(self, name, data):
+        pass
--- troup/distributed.py
+++ troup/distributed.py
@@ -1,4 +1,35 @@
+from threading import Condition
 
 class Promise:
     def __init__(self):
-        pass
+        self.error = None
+        self.__result = None
+        self.__is_done = False
+        self.__cw = Condition()
+
+    @property
+    def result(self):
+        while not self.__is_done:
+            self.wait()
+
+        if self.error:
+            raise DistributedException(self.error, self.__result)
+        return self.__result
+
+    @propery
+    def is_done(self):
+        return self.__is_done
+
+    def complete(self, result=None, error=None):
+        self.__is_done = True
+        self.error = error
+        self.result = result
+        self.__cw.notify_all()
+
+
+
+class DistributedException(Exception):
+
+    def __init__(self, message, data=None):
+        super(DistributeException, self).__init__(message)
+        self.data = data
