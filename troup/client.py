f736df7cde6f8a067a765412be46bee22366aa67
Pavle Jonoski <jonoski.pavle@gmail.com>
2016-05-04 21:14:30 +0200

    Message callback reply timeout check

--- troup/client.py
+++ troup/client.py
@@ -1,19 +1,51 @@
 from troup.infrastructure import OutgoingChannelOverWS
+from troup.distributed import Promise
+from troup.threading import IntervalTimer
 
+from datetime import datetime, timedelta
 
+class CallbackWrapper:
+    def __init__(self, callback, valid_for, promise=None, created_on=None):
+        self.callback = callback
+        self.valid_for = valid_for
+        self.created_on = created_on or datetime.now()
+        self.promise = promise or Promise()
+
+    def check_expired(self)
+        if datetime.now() > (timedelta(milliseconds=self.valid_for) + self.created_on):
+            self.promise.complete(error='Timeout', result=Exception('Timeout'))
+    def execute_callback(self, result):
+        if self.callback:
+            try:
+                self.callback(result)
+            except Exception as e:
+                print('Woops')
+        self.promise.complete(result=result)
 
 class ChannelClient:
 
-    def __init__(self, nodes_specs=None, reply_timeout=5000):
+    def __init__(self, nodes_specs=None, reply_timeout=5000, check_interval=5000):
         self.nodes_ref = {}
         self.channels = {}
-
-        self.__build._nodes_refs__(nodes_specs)
+        self.callbacks = []
+        self.reply_timeout = reply_timeout
+        self.check_interval = check_interval
+        self.maintenance_timer = self.__build_timer()
+        self.__build_nodes_refs__(nodes_specs)
 
     def __build_nodes_refs__(self, nodes_specs):
         for spec in nodes_specs:
             parsed = spec.partition(':')
             self.nodes_ref[parsed[0]] = parsed[2]
+
+    def __build_timer(self):
+        timer = IntervalTimer(interval=self.check_interval, offset=self.check_interval, target=self.__check_expired_callbacks)
+        timer.start()
+        return timer
+
+    def __check_expired_callbacks(self):
+        for wrapper in self.callbacks:
+            wrapper.check_expired()
 
     def send_message(self, message, to_node=None, on_reply=None):
         def reply_callback_wrapper(*args, **kwargs):
